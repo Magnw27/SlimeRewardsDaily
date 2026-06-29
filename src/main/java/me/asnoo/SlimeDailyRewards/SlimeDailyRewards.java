8f8376b2f63d1cc0a592a84e74487ee9da115ea9
lipcyy <ripzzbg@gmail.com>
2026-06-29 20:06:18 +0700

    Update SlimeDailyRewards.java

--- src/main/java/me/asnoo/SlimeDailyRewards/SlimeDailyRewards.java
+++ src/main/java/me/asnoo/SlimeDailyRewards/SlimeDailyRewards.java
@@ -1,1 +1,32 @@
+package me.namamu.slimerewards;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SlimeRewards extends JavaPlugin {
+
+    private static SlimeRewards instance;
+
+    @Override
+    public void onEnable() {
+
+        instance = this;
+
+        getLogger().info("--------------------------------");
+        getLogger().info("SlimeRewards Aktif Bosqu!");
+        getLogger().info("Version : " + getDescription().getVersion());
+        getLogger().info("--------------------------------");
+
+    }
+
+    @Override
+    public void onDisable() {
+
+        getLogger().info("SlimeRewards Dinonaktifkan!");
+
+    }
+
+    public static SlimeRewards getInstance() {
+        return instance;
+    }
+
+}
