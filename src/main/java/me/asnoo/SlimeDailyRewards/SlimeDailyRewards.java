99d96a1f7e7e2a76a3e222c5067902e4bc1d970b
lipcyy <ripzzbg@gmail.com>
2026-06-29 20:21:06 +0700

    Update SlimeDailyRewards.java

--- src/main/java/me/asnoo/SlimeDailyRewards/SlimeDailyRewards.java
+++ src/main/java/me/asnoo/SlimeDailyRewards/SlimeDailyRewards.java
@@ -1,8 +1,12 @@
-package me.asnoo.slimedailyrewards;
+package me.asnoo.slimerewards;
 
+import me.asnoo.slimerewards.command.DailyCommand;
+import me.asnoo.slimerewards.listener.InventoryListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SlimeRewards extends JavaPlugin {
+
+} {
 
     private static SlimeRewards instance;
 
@@ -15,6 +19,21 @@ public final class SlimeRewards extends JavaPlugin {
         getLogger().info("SlimeRewards Aktif Bosqu!");
         getLogger().info("Version : " + getDescription().getVersion());
         getLogger().info("--------------------------------");
+    @Override
+    public void onEnable() {
+
+        instance = this;
+
+        getCommand("daily").setExecutor(new DailyCommand());
+
+        getServer().getPluginManager().registerEvents(
+                new InventoryListener(),
+                this
+        );
+
+    getLogger().info("SlimeRewards Enabled!");
+
+}
 
     }
 
