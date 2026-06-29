a00cc8d4a36c9529406678a3b968c5ea72a30287
lipcyy <ripzzbg@gmail.com>
2026-06-29 20:49:44 +0700

    Update RewardsManager.java

--- src/main/java/me/asnoo/SlimeDailyRewards/manager/RewardsManager.java
+++ src/main/java/me/asnoo/SlimeDailyRewards/manager/RewardsManager.java
@@ -1,1 +1,23 @@
+package me.asnoo.slimedailyrewards.manager;
 
+import me.asnoo.slimedailyrewards.util.ItemBuilder;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+public class RewardManager {
+
+    public static ItemStack getDailyReward() {
+
+        return new ItemBuilder(Material.CHEST)
+                .name("§aDaily Reward")
+                .lore(
+                        "§7Klik untuk claim reward.",
+                        "",
+                        "§eComing Soon..."
+                )
+                .hideAttributes()
+                .build();
+
+    }
+
+}
