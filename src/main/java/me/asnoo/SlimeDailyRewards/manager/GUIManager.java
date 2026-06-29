da4a10e7e0aa2d15e10e4ceced96cdc320b0e54c
lipcyy <ripzzbg@gmail.com>
2026-06-29 20:46:42 +0700

    Create GUIManager.java

---
+++ src/main/java/me/asnoo/SlimeDailyRewards/manager/GUIManager.java
@@ -0,0 +1,16 @@
+package me.asnoo.slimedailyrewards.manager;
+
+public final class GUIManager {
+
+    private GUIManager() {
+        // Utility class
+    }
+
+    // Daily GUI
+    public static final String DAILY_TITLE = "§2✦ Daily Rewards ✦";
+    public static final int DAILY_SIZE = 27;
+
+    // Slot Reward
+    public static final int REWARD_SLOT = 13;
+
+}
