95cd1798d1f3ee42b33786fee0a4f256716afe9f
lipcyy <ripzzbg@gmail.com>
2026-06-29 20:07:38 +0700

    Update DailyCommand.java

--- src/main/java/me/asnoo/SlimeDailyRewards/command/DailyCommand.java
+++ src/main/java/me/asnoo/SlimeDailyRewards/command/DailyCommand.java
@@ -1,1 +1,23 @@
+package me.asnoo.slimerewards.command;
 
+import me.asnoo.slimerewards.gui.DailyGUI;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class DailyCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("Only players can use this command.");
+            return true;
+        }
+
+        DailyGUI.open(player);
+
+        return true;
+    }
+}
