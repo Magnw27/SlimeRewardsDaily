e05928ef9075e50090dae2aa8b7d2c7972a1cb20
lipcyy <ripzzbg@gmail.com>
2026-06-29 20:08:46 +0700

    Update DailyGui.java

--- src/main/java/me/asnoo/SlimeDailyRewards/gui/DailyGui.java
+++ src/main/java/me/asnoo/SlimeDailyRewards/gui/DailyGui.java
@@ -1,1 +1,32 @@
+package me.asnoo.slimerewards.gui;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class DailyGUI {
+
+    public static final String TITLE = "§aDaily Rewards";
+
+    public static void open(Player player) {
+
+        Inventory inventory = Bukkit.createInventory(null, 27, TITLE);
+
+        ItemStack reward = new ItemStack(Material.CHEST);
+
+        ItemMeta meta = reward.getItemMeta();
+
+        meta.setDisplayName("§aDaily Reward");
+
+        reward.setItemMeta(meta);
+
+        inventory.setItem(13, reward);
+
+        player.openInventory(inventory);
+
+    }
+
+}
