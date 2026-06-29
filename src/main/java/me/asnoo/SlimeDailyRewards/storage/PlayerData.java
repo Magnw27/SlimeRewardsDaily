115daf5411cb9d325a94770d4ec2eb34258dfb77
lipcyy <ripzzbg@gmail.com>
2026-06-29 20:54:04 +0700

    Update PlayerData.java

--- src/main/java/me/asnoo/SlimeDailyRewards/storage/PlayerData.java
+++ src/main/java/me/asnoo/SlimeDailyRewards/storage/PlayerData.java
@@ -1,1 +1,28 @@
+package me.asnoo.slimedailyrewards.storage;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class PlayerData {
+
+    private static final Map<UUID, Integer> currentDay = new HashMap<>();
+    private static final Map<UUID, Long> lastClaim = new HashMap<>();
+
+    public static int getCurrentDay(UUID uuid) {
+        return currentDay.getOrDefault(uuid, 1);
+    }
+
+    public static void setCurrentDay(UUID uuid, int day) {
+        currentDay.put(uuid, day);
+    }
+
+    public static long getLastClaim(UUID uuid) {
+        return lastClaim.getOrDefault(uuid, 0L);
+    }
+
+    public static void setLastClaim(UUID uuid, long time) {
+        lastClaim.put(uuid, time);
+    }
+
+}
